4e62cc3328a7fa9bbf684b20d2a72dad85bd45cd
auror-apeksha <apeksharajput2@gmail.com>
2023-07-03 12:55:34 +0530

    Added react-codemirror2

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,12 +1,18 @@
+import { Box, styled } from "@mui/material";
 import Editor from "./Editor";
 
+const Container = styled(Box)`
+  display: flex;
+  background-color: #060606;
+  height: 50vh;
+`;
 const Code = () => {
   return (
-    <>
-      <Editor />
-      <Editor />
-      <Editor />
-    </>
+    <Container>
+      <Editor heading="HTML" icon="/" color="#ff3c41" />
+      <Editor heading="CSS" icon="*" color="#0ebeff" />
+      <Editor heading="JavaScript" icon="()" color="#fcd000" />
+    </Container>
   );
 };
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,23 @@
 import { Box, styled } from "@mui/material";
 import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
 
+import { Controlled as ControlledEditor } from "react-codemirror2";
+
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "../App.css";
+
+const Container = styled(Box)`
+  flex-grow: 1;
+  flex-basis: 0;
+  display: flex;
+  flex-direction: column;
+  padding: 0 8px 8px;
+`;
+
 const Heading = styled(Box)`
   background: #1d1e22;
   display: flex;
@@ -15,15 +32,15 @@ const Header = styled(Box)`
   font-weight: 700;
 `;
 
-const Editor = () => {
+const Editor = ({ heading, icon, color }) => {
   return (
-    <Box>
+    <Container>
       <Header>
         <Heading>
           <Box
             component="span"
             style={{
-              backgroundColor: "red",
+              backgroundColor: color,
               display: "flex",
               width: 20,
               height: 20,
@@ -31,16 +48,23 @@ const Editor = () => {
               borderRadius: 5,
               marginRight: 5,
               paddingBottom: 2,
+              color: "#000",
             }}
           >
-            /
+            {icon}
           </Box>
-          HTML
+          {heading}
         </Heading>
         <CloseFullscreenIcon />
       </Header>
-      <Box></Box>
-    </Box>
+      <ControlledEditor
+        className="controlled-editor"
+        options={{
+          theme: "material",
+          lineNumbers: true,
+        }}
+      />
+    </Container>
   );
 };
 export default Editor;
