59635c0e52e2a5e93689613ef5f24014c7a75997
auror-apeksha <apeksharajput2@gmail.com>
2023-07-07 22:34:08 +0530

    Added reset button functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
-import { AppBar, Toolbar, styled } from "@mui/material";
+import { AppBar, Toolbar, styled, Button } from "@mui/material";
 import logo from "../assets/images/codepen-icon.png";
+import { useContext } from "react";
+import { DataContext } from "../context/DataProvider";
 
 const Container = styled(AppBar)`
   background: #060606;
   height: 10vh;
 `;
 
+const Actionbar = styled(Toolbar)`
+  display: flex;
+  justify-content: space-between;
+`;
 const Header = () => {
+  const { setHtml, setCss, setJs } = useContext(DataContext);
+  const handleReset = () => {
+    setHtml("");
+    setCss("");
+    setJs("");
+  };
   return (
     <Container position="static">
-      <Toolbar>
+      <Actionbar>
         <img src={logo} alt="logo" style={{ width: 40 }} />
-      </Toolbar>
+        <Button variant="outlined" onClick={handleReset}>
+          Reset
+        </Button>
+      </Actionbar>
     </Container>
   );
 };
